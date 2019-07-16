c33223e6297f61dce6b25661b7f980ff24395b98
Vaskka <1139851358@qq.com>
2019-07-16 20:08:58 +0800

    test index demo

--- backend/RestWeb/src/main/java/com/vaskka/fun/restweb/RestWeb/controller/IndexController.java
+++ backend/RestWeb/src/main/java/com/vaskka/fun/restweb/RestWeb/controller/IndexController.java
@@ -1,4 +1,30 @@
 package com.vaskka.fun.restweb.RestWeb.controller;
 
+import com.vaskka.fun.restweb.RestWeb.service.IndexService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+@Api(description = "首页")
+@RequestMapping(value = "/main")
+@Controller
 public class IndexController {
+
+
+    @Resource
+    private IndexService service;
+
+    @ApiOperation(value = "首页")
+    @ResponseBody
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        return service.index();
+
+    }
+
 }
--- backend/RestWeb/src/main/java/com/vaskka/fun/restweb/RestWeb/service/IndexService.java
+++ backend/RestWeb/src/main/java/com/vaskka/fun/restweb/RestWeb/service/IndexService.java
@@ -1,4 +1,12 @@
 package com.vaskka.fun.restweb.RestWeb.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class IndexService {
+
+
+    public String index() {
+        return "hello rest web";
+    }
 }
