462e364a6d138582c64b6be7324a5fbd43efd4db
Vaskka <1139851358@qq.com>
2019-07-16 17:15:12 +0800

    更新表结构

--- ddl/restweb.sql
+++ ddl/restweb.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.26, for macos10.14 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
 -- Host: localhost    Database: restweb
 -- ------------------------------------------------------
--- Server version	5.7.26
+-- Server version	8.0.15
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8mb4 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -16,38 +16,12 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `Item`
---
-
-DROP TABLE IF EXISTS `Item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Item` (
-  `id` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `businessId` varchar(255) DEFAULT NULL,
-  `price` float DEFAULT NULL,
-  `img` varchar(255) DEFAULT NULL,
-  KEY `Item_business_id_fk` (`businessId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Item`
---
-
-LOCK TABLES `Item` WRITE;
-/*!40000 ALTER TABLE `Item` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Item` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `business`
 --
 
 DROP TABLE IF EXISTS `business`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `business` (
   `id` varchar(255) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -71,7 +45,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `cart`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `cart` (
   `id` varchar(255) NOT NULL,
   `userId` varchar(255) DEFAULT NULL,
@@ -96,7 +70,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `cartitem`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `cartitem` (
   `id` varchar(255) NOT NULL,
   `userId` varchar(255) DEFAULT NULL,
@@ -114,12 +88,38 @@ LOCK TABLES `cartitem` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `comment` (
+  `id` varchar(255) NOT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `createTime` varchar(255) DEFAULT NULL,
+  `userId` varchar(255) DEFAULT NULL,
+  `orderId` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `coupn`
 --
 
 DROP TABLE IF EXISTS `coupn`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `coupn` (
   `id` varchar(255) DEFAULT NULL,
   `createTime` varchar(255) DEFAULT NULL,
@@ -140,12 +140,38 @@ LOCK TABLES `coupn` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `item` (
+  `id` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `businessId` varchar(255) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  KEY `Item_business_id_fk` (`businessId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `order`
 --
 
 DROP TABLE IF EXISTS `order`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `order` (
   `id` varchar(255) NOT NULL,
   `creatorId` varchar(255) DEFAULT NULL,
@@ -171,12 +197,11 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `orderitem`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `orderitem` (
   `id` varchar(255) NOT NULL,
   `orderId` varchar(255) DEFAULT NULL,
   `itemId` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -196,7 +221,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `session`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `session` (
   `id` varchar(255) NOT NULL,
   `token` varchar(255) DEFAULT NULL,
@@ -221,7 +246,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `transition`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `transition` (
   `id` varchar(255) NOT NULL,
   `price` float DEFAULT NULL,
@@ -247,7 +272,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `user` (
   `id` varchar(255) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -276,4 +301,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-16 12:39:04
+-- Dump completed on 2019-07-16 17:13:59
