1fe814212d63cba88575de7b669939b60d8d321d
Vaskka <1139851358@qq.com>
2019-07-17 00:31:04 +0800

    更新address表

--- ddl/dump.sql
+++ ddl/dump.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `business`;
 CREATE TABLE `business` (
   `id` varchar(255) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `headImg` varchar(255) DEFAULT NULL,
+  `headimg` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -74,7 +74,8 @@ DROP TABLE IF EXISTS `cartitem`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `cartitem` (
   `id` varchar(255) NOT NULL,
-  `userId` varchar(255) DEFAULT NULL,
+  `cartId` varchar(255) DEFAULT NULL,
+  `itemId` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -165,6 +166,7 @@ CREATE TABLE `item` (
 
 LOCK TABLES `item` WRITE;
 /*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES ('1','青菜','1',20,'https://vaskka.com/static/13945929304H50-244538.png'),('2','肉类','1',25,'https://vaskka.com/static/13945929352640-2V146.png'),('3','面筋','2',30,'https://vaskka.com/static/139459293535430-3062T.png'),('4','辣子','2',5,'https://vaskka.com/static/1394592934R5F-1VJ0.png');
 /*!40000 ALTER TABLE `item` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -294,6 +296,30 @@ LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `useraddress`
+--
+
+DROP TABLE IF EXISTS `useraddress`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `useraddress` (
+  `id` varchar(255) NOT NULL,
+  `userId` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `useraddress`
+--
+
+LOCK TABLES `useraddress` WRITE;
+/*!40000 ALTER TABLE `useraddress` DISABLE KEYS */;
+/*!40000 ALTER TABLE `useraddress` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -304,4 +330,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-16 21:20:36
+-- Dump completed on 2019-07-17  0:30:35
