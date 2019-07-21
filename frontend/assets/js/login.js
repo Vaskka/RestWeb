5c5a347117da98bdffcbc3e3da0cd1feb32c2bee
Vaskka <1139851358@qq.com>
2019-07-21 13:31:36 +0800

    更改登陆逻辑

--- frontend/assets/js/login.js
+++ frontend/assets/js/login.js
@@ -1,5 +1,3 @@
-var showTag = true;
-
 $(function() {
 
    $(".input input").focus(function() {
@@ -49,29 +47,80 @@ $(function() {
       $("button", this).addClass('active');
    })
 
-   $(".shape").click(function() {
-         if (showTag) {
-            $(".box").css("opacity", 0);
-            $(".overbox").css("opacity", 1);
-            $(".overbox").css("background-color", "#AAAAAA");
+   $(".alt-2").click(function() {
+      if (!$(this).hasClass('material-button')) {
+         $(".shape").css({
+            "width": "100%",
+            "height": "100%",
+            "transform": "rotate(0deg)"
+         })
 
+         setTimeout(function() {
+            $(".overbox").css({
+               "overflow": "initial"
+            })
+         }, 600)
 
-         }
-         else {
-            $(".box").css("opacity", 1);
-            $(".overbox").css("opacity", 0);
+         $(this).animate({
+            "width": "140px",
+            "height": "140px"
+         }, 500, function() {
+            $(".box").removeClass("back");
 
+            $(this).removeClass('active')
+         });
 
+         $(".overbox .title").fadeOut(300);
+         $(".overbox .input").fadeOut(300);
+         $(".overbox .button").fadeOut(300);
 
-         }
-         showTag = !showTag;
+         $(".alt-2").addClass('material-buton');
+      }
+
+   })
+
+   $(".material-button").click(function() {
+
+      if ($(this).hasClass('material-button')) {
+         setTimeout(function() {
+            $(".overbox").css({
+               "overflow": "hidden"
+            })
+            $(".box").addClass("back");
+         }, 200)
+         $(this).addClass('active').animate({
+            "width": "700px",
+            "height": "700px"
+         });
+
+         setTimeout(function() {
+            $(".shape").css({
+               "width": "50%",
+               "height": "50%",
+               "transform": "rotate(45deg)"
+            })
+
+            $(".overbox .title").fadeIn(300);
+            $(".overbox .input").fadeIn(300);
+            $(".overbox .button").fadeIn(300);
+         }, 700)
+
+         $(this).removeClass('material-button');
+
+      }
+
+      if ($(".alt-2").hasClass('material-buton')) {
+         $(".alt-2").removeClass('material-buton');
+         $(".alt-2").addClass('material-button');
+      }
+
    });
 
 });
 
 function login() {
    let phone = document.getElementById("phone").value;
-   let password = document.getElementById("password").value;
+   let password = document.getElementById("pass").value;
 
    get("/user/login", [phone, password], function (data) {
 
@@ -91,9 +140,9 @@ function login() {
 
 function register() {
    let phone = document.getElementById("regphone").value;
-   let password = document.getElementById("regpasswordpass").value;
+   let password = document.getElementById("regpass").value;
    let name = document.getElementById("regname").value;
-   let repassword = document.getElementById("reregpassword").value;
+   let repassword = password;
 
    get("/user/register", [phone, name, password, repassword, 0], function (data) {
 
